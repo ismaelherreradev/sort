7dfee3dbfc264fcbb3b61f6670a788f10a92ed29
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-04-28 21:23:14 -0400

    Change sort to abstract class

--- dist/Sorter.js
+++ dist/Sorter.js
@@ -2,16 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
     sort() {
-        const { length } = this.collection;
+        const { length } = this;
         for (let i = 0; i < length; i++) {
             let noSwap = true;
             for (let j = -1; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                     noSwap = false;
                 }
             }
--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,24 @@ import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 const numbersCollections = new NumberCollection([10, 3, -5, 0, 100, 1, 2, 3, 4, 5]);
+
+console.log(numbersCollections.data);
+
+numbersCollections.sort();
+
+console.log(numbersCollections.data);
+
+
 const charactersCollection = new CharactersCollection("Xaayb");
 
-
-const numberSorted = new Sorter(numbersCollections);
-console.log(numbersCollections.data);
-
-numberSorted.sort();
-
-console.log(numbersCollections.data);
-
-const charactersSorted = new Sorter(charactersCollection);
 console.log(charactersCollection.data);
 
-charactersSorted.sort();
+charactersCollection.sort();
 
 console.log(charactersCollection.data);
 
 
 const linkedList = new LinkedList();
-const linkedListSorted = new Sorter(linkedList);
 
 linkedList.add(500);
 linkedList.add(-10);
@@ -34,6 +32,6 @@ linkedList.add(4);
 
 console.log(linkedList.print());
 
-linkedListSorted.sort();
+linkedList.sort();
 
 console.log(linkedList.print());
